4353f3eada127dc63096b3b3831a55b707a413a7
C0smos <wes.renshaw@student.shu.ac.uk>
2016-02-09 19:27:21 +0000

    Added gui, and switched to dev branch

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,53 @@
-class gui
+#!/usr/bin/env python
+import sys
+from translate import *
+from PySide.QtCore import *
+from PySide.QtGui import *
 
-  def buttons():
-      pass
+# Remove this after
+qt_app = QApplication(sys.argv)
+
+class gui(QLabel):
+  
+  def __init__(self):
+      trans = Trans()
+
+      QLabel.__init__(self, "No idea")
+      self.sal_lbl = QLabel("yoyoyo", self)
+      #self.sal_lbl.move(5, 40)
+      self.sal_lbl.setAlignment(Qt.AlignLeft)
+      self.sals = ['ahoy',
+                  'fsdf',
+                  'cuntbubble',
+                  'tryingg',
+                  'fkfkfksdf',
+                  'yo',
+                  'wassup']
+      self.sal = QComboBox(self)
+      self.sal.addItems(self.sals)
+      #self.sal.move(110, 50)
+
+      self.setMinimumSize(QSize(600, 400))
+      self.setAlignment(Qt.AlignCenter)
+      self.setWindowTitle("Yo")
+      self.btn = QPushButton('Build Greeting', self)
+      self.btn.move(5, 40)
+      self.btn.clicked.connect(lambda: trans.koreanLan(self.btn.()))
+
+
+  def run(self):
+      self.show()
+      qt_app.exec_()
+
+'''
+  def cunter():
+
+      qt_app = QApplication(sys.argv)
+      label = QLabel("GFuckkfkfkfkfkf!>!>!>!>!>.. ")
+
+      label.show()
+
+      qt_app.exec_()
+'''
+app = gui()
+app.run()
--- src/translate.py
+++ src/translate.py
@@ -8,8 +8,18 @@ class Trans():
 
         pass
 
-    def 
+    def koreanLan(self, name):
+        lists = ["hello", "btn", "kimchi"]
+        print name
+        '''
+        for word in lists:
+            if word in name:
+                print name
+        '''
+
+        
     
     def wordTrans(origWord, lang):
+        pass
+
         # Translates 
-        
